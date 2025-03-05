5f5d4b27f77cac0b18dfe96481683bd1e82bec88
Puneeth S <bubblypuneeth@gmail.com>
2025-03-05 14:31:55 +0530

    Add files via upload

---
+++ oops 3.py
@@ -0,0 +1,11 @@
+class Human:
+    def __init__(self,name):
+        self.name = name
+
+        def walk(self):
+            print(f"{ self.name } is walking")     
+c=Human("Puneeth")
+c.walk()
+
+    
+    
